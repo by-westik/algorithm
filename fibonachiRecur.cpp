489bc4879e1cba8d40fefde94bb28bd33a4ac5a2
by-westik <78916184+by-westik@users.noreply.github.com>
2022-07-11 22:25:56 +0300

    Create fibonachiRecur.cpp

---
+++ fibonachiRecur.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+unsigned long fibonachi(unsigned long n){
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    return fibonachi(n - 1) + fibonachi(n - 2);
+};
+
+int main(){
+    int n;
+    std::cout << "Введите номер числа Фибоначчи" << std::endl;
+    std::cin >> n;
+    std::cout << n << "-ое число Фибоначии это " << fibonachi(n) << std::endl;
+    return 0;
+};
